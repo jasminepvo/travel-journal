186ebd976e5b2cc7841dd0ff3c7c9064220085db
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-09-24 17:11:40 -0400

    Add sample data and import to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,26 @@ import './App.css'
 import './components/Navbar.css'
 import Navbar from './components/Navbar'
 import TravelCard from './components/TravelCard'
+import Data from './Data'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const cards = Data.map(card => {
+    return (
+      <TravelCard
+        key={card.id}
+        {...card}
+      />
+    )
+  })
+
   return (
     <div className="App">
       <Navbar className="Navbar"/>
-      <TravelCard />
+      <section className='cards-list'>
+        {cards}
+      </section>
 
       {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
---
+++ src/Data.jsx
@@ -0,0 +1,44 @@
+export default [
+  {
+    id: 1,
+    coverImg: "https://images.unsplash.com/photo-1510097467424-192d713fd8b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1400&q=80",
+    title: "Cancun",
+    location: {
+      name: "MEXICO",
+      link: "",
+    },
+    dates: {
+      startDate: "14 Jan, 2021",
+      endDate: "24 Jan, 2021"
+    },
+    description: "Visited Cancun with family and stayed in an all-inclusive resort. Swam with dolphins, jet-skied to the coral reef, laid in the sand for hours, explored the local shops,  and ate buffet for breakfast, lunch and dinner every day.",
+  },
+  {
+    id: 2,
+    coverImg: "https://images.unsplash.com/photo-1510097467424-192d713fd8b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1400&q=80",
+    title: "Cancun",
+    location: {
+      name: "MEXICO",
+      link: "",
+    },
+    dates: {
+      startDate: "14 Jan, 2021",
+      endDate: "24 Jan, 2021"
+    },
+    description: "Visited Cancun with family and stayed in an all-inclusive resort. Swam with dolphins, jet-skied to the coral reef, laid in the sand for hours, explored the local shops,  and ate buffet for breakfast, lunch and dinner every day.",
+  },
+  {
+    id: 3,
+    coverImg: "https://source.unsplash.com/3PeSjpLVtLg",
+    title: "Cancun",
+    location: {
+      name: "MEXICO",
+      link: "",
+    },
+    dates: {
+      startDate: "14 Jan, 2021",
+      endDate: "24 Jan, 2021"
+    },
+    description: "Visited Cancun with family and stayed in an all-inclusive resort. Swam with dolphins, jet-skied to the coral reef, laid in the sand for hours, explored the local shops,  and ate buffet for breakfast, lunch and dinner every day.",
+  },
+]
