a82d2ea0cee87eb8a0c47fc4d2a18afc6179e788
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-09-24 15:59:18 -0400

    Configure files and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
+import TravelCard from './components/TravelCard'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <Navbar />
+      <TravelCard />
 
       {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
